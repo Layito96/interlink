5935494edb5032ca0145fd71d88b86eb0227663e
Layito96 <50017226+Layito96@users.noreply.github.com>
2024-09-20 11:10:06 +0000

    Update constants.js

--- app/_utiles/constants.js
+++ app/_utiles/constants.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://192.168.1.55:1338";
-// const BASE_URL = "http://localhost:1338";
+// const BASE_URL = "http://localhost:1338"; local
 const BASE_URL = "https://cms-interlink.onrender.com";
 
 export default BASE_URL;
